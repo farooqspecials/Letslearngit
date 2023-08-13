9c3f3ad5813edf3d68eb61142bb534877a7a7026
Farooq Abdullah <farooqspecials@gmail.com>
2023-08-13 17:36:16 +0500

    multiply feature

---
+++ multiply.java
@@ -0,0 +1,12 @@
+class Multiply {
+
+    public static void main(String[] args) {
+
+        int first = 10;
+        int second = 20;
+
+        // add two numbers
+        int multiply = first * second;
+        System.out.println("The Multipy is " + first + " x " + second + " = " + multiply);
+    }
+}
